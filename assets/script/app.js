20b5103ee11eaa8c07555277746165e692257479
SusannaParadi <105036353+SusannaParadi@users.noreply.github.com>
2022-06-18 10:55:10 +0200

    Add files via upload

---
+++ assets/script/app.js
@@ -0,0 +1,134 @@
+//Grab couple of things we need
+const section = document.querySelector(".js-cards");
+let playerTryCount = document.querySelector("span");
+let playerTry = 0;
+
+//Link text
+playerTryCount.textContent = playerTry;
+
+//Generate Data
+
+const getData = ()  => [
+    {imgSrc: "./assets/img/cake.jpg", name: "cake"},
+    {imgSrc: "./assets/img/cake2.jpg", name: "cake2"},
+    {imgSrc: "./assets/img/cake3.jpg", name: "cake3"},
+    {imgSrc: "./assets/img/cake4.jpg", name: "cake4"},
+    {imgSrc: "./assets/img/cake5.jpg", name: "cake5"},
+    {imgSrc: "./assets/img/cake6.jpg", name: "cake6"},
+    {imgSrc: "./assets/img/cake7.jpg", name: "cake7"},
+    {imgSrc: "./assets/img/cake8.jpg", name: "cake8"},
+    {imgSrc: "./assets/img/cake.jpg", name: "cake"},
+    {imgSrc: "./assets/img/cake2.jpg", name: "cake2"},
+    {imgSrc: "./assets/img/cake3.jpg", name: "cake3"},
+    {imgSrc: "./assets/img/cake4.jpg", name: "cake4"},
+    {imgSrc: "./assets/img/cake5.jpg", name: "cake5"},
+    {imgSrc: "./assets/img/cake6.jpg", name: "cake6"},
+    {imgSrc: "./assets/img/cake7.jpg", name: "cake7"},
+    {imgSrc: "./assets/img/cake8.jpg", name: "cake8"},
+]
+
+
+//Randomize
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() -0.5); 
+    return cardData;
+};
+
+randomize();
+
+//Card Generator Function
+const cardGenerator = () => {
+    const cardData = randomize();
+    //Generate the HTML
+    const cards = document.querySelectorAll(".card");
+    cardData.forEach((item, index) => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList ="card";
+        face.classList ="face";
+        back.classList ="back";
+        //Attach the info to the cards
+        face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
+        //Attach the cards to the section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener('click', (e) => {
+            card.classList.toggle('toggleCard');
+            checkCards(e);
+        });
+    });
+};
+
+
+const checkCards = (e) => {
+    const clickedCard = e.target;
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll(".flipped");
+    if (flippedCards.length === 2) {
+        section.style.pointerEvents ="none";
+    } 
+    const toggleCard = document.querySelectorAll(".toggleCard");
+    
+    
+    //Logic
+    if (flippedCards.length === 2) {
+        if (
+            flippedCards[0].getAttribute("name") ===
+            flippedCards[1].getAttribute("name")
+            ) {
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents ="none";
+            });
+
+        } else {
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                setTimeout(() => card.classList.remove("toggleCard"), 2000);      
+            });
+        }
+        setTimeout( () => {
+            section.style.pointerEvents ="auto";
+        }, 2000); 
+
+        playerTry++;
+        playerTryCount.textContent = playerTry;
+        if (toggleCard.length === 16) {
+            setTimeout( () => {
+                modal.style.display ="block";
+            }, 1500);
+        }
+
+    }    
+};
+
+cardGenerator();
+
+//Restart
+const restart = () => {
+    location.reload();
+}
+
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+function again() {
+    modal.style.display = "none";
+    restart();
+}
