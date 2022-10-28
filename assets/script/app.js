b4bc2927e40c46b89272756e5a5cb1b67875c1c1
SusannaParadi <105036353+SusannaParadi@users.noreply.github.com>
2022-10-28 19:10:03 +0200

    Update app.js

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,5 @@
-//Grab couple of things we need
-const section = document.querySelector(".js-cards");
+//Grab couple of things I need
+const section = document.querySelector(".game");
 let playerTryCount = document.querySelector("span");
 let playerTry = 0;
 
@@ -9,24 +9,25 @@ playerTryCount.textContent = playerTry;
 //Generate Data
 
 const getData = ()  => [
-    {imgSrc: "./assets/img/cake.jpg", name: "cake"},
-    {imgSrc: "./assets/img/cake2.jpg", name: "cake2"},
-    {imgSrc: "./assets/img/cake3.jpg", name: "cake3"},
-    {imgSrc: "./assets/img/cake4.jpg", name: "cake4"},
-    {imgSrc: "./assets/img/cake5.jpg", name: "cake5"},
-    {imgSrc: "./assets/img/cake6.jpg", name: "cake6"},
-    {imgSrc: "./assets/img/cake7.jpg", name: "cake7"},
-    {imgSrc: "./assets/img/cake8.jpg", name: "cake8"},
-    {imgSrc: "./assets/img/cake.jpg", name: "cake"},
-    {imgSrc: "./assets/img/cake2.jpg", name: "cake2"},
-    {imgSrc: "./assets/img/cake3.jpg", name: "cake3"},
-    {imgSrc: "./assets/img/cake4.jpg", name: "cake4"},
-    {imgSrc: "./assets/img/cake5.jpg", name: "cake5"},
-    {imgSrc: "./assets/img/cake6.jpg", name: "cake6"},
-    {imgSrc: "./assets/img/cake7.jpg", name: "cake7"},
-    {imgSrc: "./assets/img/cake8.jpg", name: "cake8"},
+    {imgSrc: "./img/cake.jpg", name: "cake"},
+    {imgSrc: "./img/cake2.jpg", name: "cake2"},
+    {imgSrc: "./img/cake3.jpg", name: "cake3"},
+    {imgSrc: "./img/cake4.jpg", name: "cake4"},
+    {imgSrc: "./img/cake5.jpg", name: "cake5"},
+    {imgSrc: "./img/cake6.jpg", name: "cake6"},
+    {imgSrc: "./img/cake7.jpg", name: "cake7"},
+    {imgSrc: "./img/cake8.jpg", name: "cake8"},
+    {imgSrc: "./img/cake.jpg", name: "cake"},
+    {imgSrc: "./img/cake2.jpg", name: "cake2"},
+    {imgSrc: "./img/cake3.jpg", name: "cake3"},
+    {imgSrc: "./img/cake4.jpg", name: "cake4"},
+    {imgSrc: "./img/cake5.jpg", name: "cake5"},
+    {imgSrc: "./img/cake6.jpg", name: "cake6"},
+    {imgSrc: "./img/cake7.jpg", name: "cake7"},
+    {imgSrc: "./img/cake8.jpg", name: "cake8"},
 ]
 
+//const data = getData();
 
 //Randomize
 const randomize = () => {
@@ -64,54 +65,71 @@ const cardGenerator = () => {
     });
 };
 
+//Check cards
 
 const checkCards = (e) => {
     const clickedCard = e.target;
     clickedCard.classList.add("flipped");
     const flippedCards = document.querySelectorAll(".flipped");
-    if (flippedCards.length === 2) {
-        section.style.pointerEvents ="none";
-    } 
     const toggleCard = document.querySelectorAll(".toggleCard");
     
     
     //Logic
     if (flippedCards.length === 2) {
+        section.style.pointerEvents = 'none';
         if (
             flippedCards[0].getAttribute("name") ===
             flippedCards[1].getAttribute("name")
             ) {
+            console.log("match")
             flippedCards.forEach((card) => {
                 card.classList.remove("flipped");
                 card.style.pointerEvents ="none";
+                setTimeout(() => section.style.pointerEvents = 'unset', 2000); 
             });
 
         } else {
+            console.log("wrong")
             flippedCards.forEach((card) => {
                 card.classList.remove("flipped");
-                setTimeout(() => card.classList.remove("toggleCard"), 2000);      
+                setTimeout(() => card.classList.remove("toggleCard"), 2000);
+                setTimeout(() => section.style.pointerEvents = 'unset', 2000);              
             });
         }
-        setTimeout( () => {
-            section.style.pointerEvents ="auto";
-        }, 2000); 
-
         playerTry++;
         playerTryCount.textContent = playerTry;
+        if (playerTry === 500) {
+            restart();
+        } 
         if (toggleCard.length === 16) {
             setTimeout( () => {
                 modal.style.display ="block";
             }, 1500);
         }
-
-    }    
+    }   
+    
 };
 
 cardGenerator();
 
 //Restart
 const restart = () => {
-    location.reload();
+    let cardData =randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents ="none";
+    cardData.forEach((item,index)=> {
+        cards[index].classList.remove("toggleCard");
+        //Randomize
+        setTimeout( () => {
+        cards[index].style.pointerEvents = "unset";
+        faces[index].src = item.imgSrc;
+        cards[index].setAttribute("name", item.name);
+        section.style.pointerEvents ="unset";
+     }, 1000);       
+    })
+    playerTry = 0;
+    playerTryCount.textContent = playerTry;
 }
 
 const modal = document.getElementById("myModal");
@@ -125,10 +143,4 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
-
-
-function again() {
-    modal.style.display = "none";
-    restart();
 }
